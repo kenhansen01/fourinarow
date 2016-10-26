e3b14fd78ee65917878c2d0f2df3c71aabb09bd3
Hansen <KEH0628@daugherty.com>
2016-10-26 09:01:53 -0500

    change post to return id string

--- routes/player.ts
+++ routes/player.ts
@@ -8,33 +8,41 @@ let db = new MongoUtils('dbsConnectFour', 'players', 'mongodb://localhost:27017/
 /**
  * Get all players from database
  */
-router.get('/players', (req, res, next) => {
-  db.getAll().subscribe(playersResponse =>
-    res.json(playersResponse),
-    err => res.send(err));
-});
+router.get('/players', (req, res, next) =>
+  db.getAll()
+    .subscribe(
+    playersResponse => res.json(playersResponse),
+    err => res.send(err))
+);
 
 /**
  * Get a specific player by id
  */
-router.get('/player/:id', (req, res, next) => {
-  db.getOneById(req.params.id).subscribe(playerResponse => res.json(playerResponse), err => res.send(err));
-});
+router.get('/player/:id', (req, res, next) =>
+  db.getOneById(req.params.id)
+    .subscribe(
+    playerResponse => res.json(playerResponse),
+    err => res.send(err))
+);
 
 /**
  * Save new player to database
  */
-router.post('/player', (req, res, next) => {
+router.post('/player', (req, res, next) =>
   db.saveItem(req.body)
-    .subscribe(saveRes =>
-      res.json(saveRes),
-      err => res.send(err));
-});
+    .subscribe(
+    saveRes => res.json({ _id: saveRes.insertedId.toHexString() }),
+    err => res.send(err))
+);
 
 /**
  * Delete player from database
  */
-router.delete('/player/:id', (req, res, next) => db.deleteItemById(req.params.id).subscribe(deleteRes => res.json(deleteRes), err => res.send(err)));
+router.delete('/player/:id', (req, res, next) => db.deleteItemById(req.params.id)
+  .subscribe(
+  deleteRes => res.json(deleteRes),
+  err => res.send(err))
+);
 
 /**
  * Update an existing player in database
@@ -49,7 +57,10 @@ router.put('/player/:id', (req, res, next) => {
     experience: req.body.experience,
     jobTitle: req.body.jobTitle
   };
-  db.updateItem(req.body._id, player).subscribe(updateRes => res.json(updateRes), err => res.send(err));
+  db.updateItem(req.body._id, player)
+    .subscribe(
+    updateRes => res.json(updateRes),
+    err => res.send(err));
 });
 
 export = router;
